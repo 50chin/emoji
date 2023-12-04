3953f6e90818cdf344559a4cbdf9b9acd0c146cd
50chin <50chin50@gmail.com>
2023-12-04 16:21:16 +0300

    emoji project

---
+++ script/app.js
@@ -0,0 +1,41 @@
+import { cards } from './emoji.js';
+
+const cardsWrapper = document.querySelector('.section__cards');
+
+const filterWords = (str) => {
+  return [...new Set(str.split(' '))].join(' ');
+};
+
+const input = document.querySelector('.input');
+input.addEventListener('input', () => {
+  const filteredCards = cards.filter(
+    (key) =>
+      key.keywords.includes(input.value.toLowerCase().trim()) ||
+      key.title.includes(input.value.toLowerCase().trim())
+  );
+  renderCards(filteredCards);
+});
+
+const createCard = ({ title, symbol, keywords }) => {
+  const card = document.createElement('div');
+  card.className = 'card';
+
+  card.innerHTML = `<h3 class = card__icon> ${symbol} </h3>
+  <p class = "card__name">${title}</p>
+  <p class = "">${filterWords(keywords)}</p>`;
+  return card;
+};
+
+const keyWor = document.querySelector('.card__text');
+
+const renderCards = (cards) => {
+  cardsWrapper.innerHTML = '';
+  cards.forEach((icon) => {
+    const card = createCard(icon);
+    cardsWrapper.append(card);
+  });
+};
+
+renderCards(cards);
+
+document.getElementById('year').innerHTML = new Date().getFullYear();
