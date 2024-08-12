2d63130e5b193569693c9b0be5d8496b507a43ea
50chin <50chin50@gmail.com>
2024-08-12 14:59:14 +0300

    added emoji file

--- src/script/app.js
+++ src/script/app.js
@@ -1,4 +1,7 @@
-const API_URL = `http://api.codeoverdose.space/api/emoji/v1/find/?query=`;
+// const API_URL = `http://api.codeoverdose.space/api/emoji/v1/find/?query=`;
+
+import { data } from './emoji.js';
+// console.log(data);
 
 const cardsWrapper = document.querySelector('.section__cards');
 
@@ -6,26 +9,30 @@ const filterWords = (str) => {
   return [...new Set(str.split(' '))].join(' ');
 };
 
-const getData = async (url, query = '') => {
-  try {
-    const res = await fetch(url + query);
-    return res.json();
-  } catch (err) {
-    console.log(err);
-  }
-};
+// const getData = async (url, query = '') => {
+//   try {
+//     const res = await fetch(url + query);
+//     return res.json();
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
 
-const cards = await getData(API_URL);
+// // const cards = await getData(API_URL);
 
 const input = document.querySelector('.input');
-input.addEventListener('input', async () => {
-  const cards = await getData(API_URL, input.value);
-
-  renderCards(cards);
+input.addEventListener('input', (evt) => {
+  const text = evt.target.value;
+  const search = data.filter(
+    (el) =>
+      el.keywords.toLowerCase().includes(text.toLowerCase().trim()) ||
+      el.title.toLowerCase().includes(text.toLowerCase().trim())
+  );
+  renderCards(search);
 });
 
 const createCard = ({ title, symbol, keywords }) => {
-  const card = document.createElement('div');
+  const card = document.createElement('article');
   card.className = 'card';
 
   card.innerHTML = `<h3 class = card__icon> ${symbol} </h3>
@@ -34,14 +41,14 @@ const createCard = ({ title, symbol, keywords }) => {
   return card;
 };
 
-const renderCards = (cards) => {
+const renderCards = (data) => {
   cardsWrapper.innerHTML = '';
-  cards.forEach((icon) => {
+  data.forEach((icon) => {
     const card = createCard(icon);
     cardsWrapper.append(card);
   });
 };
 
-renderCards(cards);
+renderCards(data);
 
 document.getElementById('year').innerHTML = new Date().getFullYear();
